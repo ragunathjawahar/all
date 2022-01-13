37fd488a2b00b3671480ccc39412b3aec25c6ed7
Ragunath Jawahar <ragunath@redgreen.io>
2022-01-13 15:19:07 +0530

    test: add test for object stream with one property

--- src/test/kotlin/xyz/ragunath/objectstreams/ObjectStreamTest.kt
+++ src/test/kotlin/xyz/ragunath/objectstreams/ObjectStreamTest.kt
@@ -32,16 +32,30 @@ class ObjectStreamTest {
 
     @Test
     fun `value objects`() {
-      // given & when
-      val ages = ObjectStream
+      // given
+      val agesBuilder = ObjectStream
         .of(Age::class)
         .property("value", 1, 2, 3)
-        .generate()
+
+      // when
+      val ages = agesBuilder.generate()
 
       // then
       assertThat(ages)
         .containsExactly(Age(1), Age(2), Age(3))
         .inOrder()
+    }
+
+    @Test
+    fun `age with one value`() {
+      // given
+      val ageBuilder = ObjectStream
+        .of(Age::class)
+        .property("value", 1)
+
+      // when & then
+      assertThat(ageBuilder.generate())
+        .containsExactly(Age(1))
     }
   }
 }
