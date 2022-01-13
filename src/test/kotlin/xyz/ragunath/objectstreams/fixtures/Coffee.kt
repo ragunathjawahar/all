62d8807133642f3ed8e57220af60fadfc754b04e
Ragunath Jawahar <ragunath@redgreen.io>
2022-01-13 15:14:20 +0530

    feat: add a new fixture (`Coffee`)

---
+++ src/test/kotlin/xyz/ragunath/objectstreams/fixtures/Coffee.kt
@@ -0,0 +1,15 @@
+package xyz.ragunath.objectstreams.fixtures
+
+enum class Roast {
+  Light, Medium, Dark
+}
+
+enum class GrindSize {
+  Fine, MediumFine, Medium, Coarse
+}
+
+data class Coffee(
+  val estate: String,
+  val roast: Roast,
+  val grindSize: GrindSize
+)
