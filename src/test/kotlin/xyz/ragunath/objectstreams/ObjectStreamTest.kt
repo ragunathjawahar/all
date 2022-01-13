71e7449248bf55bfdc911d03c1262ac63db49158
Ragunath Jawahar <ragunath@redgreen.io>
2022-01-13 14:34:00 +0530

    refactor: convert to nested test

--- src/test/kotlin/xyz/ragunath/objectstreams/ObjectStreamTest.kt
+++ src/test/kotlin/xyz/ragunath/objectstreams/ObjectStreamTest.kt
@@ -1,17 +1,21 @@
 package xyz.ragunath.objectstreams
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class ObjectStreamTest {
-  @Test
-  fun `single parameter`() {
-    // given & when
-    val intStream = ObjectStream.of(1, 2, 3)
+  @Nested
+  inner class SingleParameters {
+    @Test
+    fun integers() {
+      // given & when
+      val intStream = ObjectStream.of(1, 2, 3)
 
-    // then
-    assertThat(intStream)
-      .containsExactly(1, 2, 3)
-      .inOrder()
+      // then
+      assertThat(intStream)
+        .containsExactly(1, 2, 3)
+        .inOrder()
+    }
   }
 }
